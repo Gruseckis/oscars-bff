4896cc9968ed1bc9dbf35ade43d9f642abdc5100
Germans Gruseckis <g.gruseckis@gmail.com>
2020-05-24 18:45:11 +0300

    Small refactoring

--- src/routes/reservationRouter.ts
+++ src/routes/reservationRouter.ts
@@ -3,18 +3,18 @@ import { reservationController } from '../controllers/reservationController';
 
 const router = express.Router();
 
-router.post('/:id/quote', reservationController.quoteRepair.bind(reservationController));
+router.post('/:id/quote', reservationController.quoteRepair);
 
-router.get('/:id/approve', reservationController.aproveReservation.bind(reservationController));
+router.get('/:id/approve', reservationController.aproveReservation);
 
 router.post('/:id/complete');
 
-router.get('/find', reservationController.findItem.bind(reservationController));
+router.get('/find', reservationController.findItem);
 
-router.get('/new', reservationController.getPendingReservations.bind(reservationController));
+router.get('/new', reservationController.getPendingReservations);
 
-router.post('/check', reservationController.checkReservation.bind(reservationController));
+router.post('/check', reservationController.checkReservation);
 
-router.post('/reserve', reservationController.makeReservation.bind(reservationController));
+router.post('/reserve', reservationController.makeReservation);
 
 export default router;
